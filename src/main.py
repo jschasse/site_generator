bdc31d6d8fbc0804d70cab9a6954f7e2f04936d4
jschasse <jaredchasse4@msn.com>
2025-01-20 18:52:35 -0500

    added classes texttype and textnode. added unittests for testnode classes

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from textnode import *
+
+def main():
+    test = TextNode("hello wanderer", TextType.BOLD, "https://www.boot.dev")
+    print(test.__repr__())
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
