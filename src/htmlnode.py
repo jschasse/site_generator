ef87117497a9002a1dd259a00886542c96bfbede
jschasse <jaredchasse4@msn.com>
2025-01-21 21:42:59 -0500

    added LeafNode child class from HTMLNode and added tests for its to_html method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -16,3 +16,16 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode: tag:{self.tag}, value:{self.value}, props{self.props}, children:{self.children}"
+    
+class LeafNode(HTMLNode):
+    
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return f"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -7,11 +7,14 @@ def main():
     data3 = {"fdsfgdsfg": "sdfsdgsdgsdg"}
     node = HTMLNode("p", "im goku", data2)
     node2 = HTMLNode("p", "im goku",  data)
-    node3 = HTMLNode("p", "im goku",  data3)    
+    node3 = LeafNode("p", "This is a paragraph of text.")
+    node4 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})  
     print(node.__repr__())
     print(node2.__repr__())
     print(node3.__repr__())
     print(node.props_to_html())
+    print(node3.to_html())
+    print(node4.to_html())
 
 
 if __name__ == "__main__":
