985532e272156ac4df47dfa37a01b088ab18ab6a
jschasse <jaredchasse4@msn.com>
2025-01-28 17:14:18 -0500

    added recursive function to print html nodes with parent and leaf nodes. Also added testing for the function

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,6 +1,6 @@
 class HTMLNode():
     
-    def __init__(self, tag=None, value=None, props=None, children=None):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
@@ -20,12 +20,31 @@ class HTMLNode():
 class LeafNode(HTMLNode):
     
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, props)
+        super().__init__(tag, value, None, props)
     
     def to_html(self):
         if self.value == None:
-            raise ValueError
+            raise ValueError("No value")
         if self.tag == None:
             return f"{self.value}"
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+        
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("No tag")
+        if self.children == None:
+            raise ValueError("Children cannot be None")
+        
+        html = "".join(child.to_html() for child in self.children)
+            
+        return f"<{self.tag}>{html}</{self.tag}>"
+            
+            
+            
+        
         
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,19 +2,27 @@ from htmlnode import *
 
         
 def main():
-    data = {"href": "https://www.google.com"}
-    data2 = {"href": "https://www.google.com", "target": "_blank"}
-    data3 = {"fdsfgdsfg": "sdfsdgsdgsdg"}
-    node = HTMLNode("p", "im goku", data2)
-    node2 = HTMLNode("p", "im goku",  data)
-    node3 = LeafNode("p", "This is a paragraph of text.")
-    node4 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})  
-    print(node.__repr__())
-    print(node2.__repr__())
-    print(node3.__repr__())
-    print(node.props_to_html())
-    print(node3.to_html())
-    print(node4.to_html())
+    node = ParentNode(
+    "p",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+        ParentNode(
+    "q",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ],
+)
+
+    ],
+    )
+
+    print(node.to_html())
 
 
 if __name__ == "__main__":
